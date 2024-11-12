7a144f301c3ac76da440e833a29799523a3a0154
juancho1804 <juanmanuelceron@unicauca.edu.co>
2024-11-12 12:03:28 -0500

    Primer commit

---
+++ competencias-rap-service/src/main/java/org/unicauca/competenciasrapservice/CompetenciasRapServiceApplication.java
@@ -0,0 +1,13 @@
+package org.unicauca.competenciasrapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetenciasRapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetenciasRapServiceApplication.class, args);
+	}
+
+}
