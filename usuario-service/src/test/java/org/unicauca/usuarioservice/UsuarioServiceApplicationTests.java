901642592bfa5bb7ed66cfc751857946dfd92159
juancho1804 <juanmanuelceron@unicauca.edu.co>
2024-11-21 22:16:58 -0500

    Primer commit rap y competencias por programa

---
+++ usuario-service/src/test/java/org/unicauca/usuarioservice/UsuarioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unicauca.usuarioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
