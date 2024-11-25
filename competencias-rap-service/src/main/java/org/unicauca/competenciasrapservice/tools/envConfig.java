57eec36ec8ce0c30edd8f0cb1b9a8e26a39acf96
Oscar Fernando Hoyos Carvajal <57969765+Viperexz@users.noreply.github.com>
2024-11-25 15:17:29 -0500

    Ajustes tecnicos.
    Se organizo la estructura de los proyectos usando los pom.xml
    
    Se establece la cadena de conexion general para una sola BD.
    
    Se  instalan la dependencia para los actuadores.
    
    Se inicio con las variables de entorno .env
    
    NOTA: los .env no se subiran al repositorio son propios de cada equipo

---
+++ competencias-rap-service/src/main/java/org/unicauca/competenciasrapservice/tools/envConfig.java
@@ -0,0 +1,23 @@
+package org.unicauca.competenciasrapservice.tools;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class envConfig {
+    static {
+        // Ruta relativa al archivo .env
+        String projectRoot = new java.io.File("../BackendSW3").getAbsolutePath();
+        System.out.println("Using dotenv path: " + projectRoot);
+
+        // Configurar dotenv para cargar las variables desde el archivo .env
+        Dotenv dotenv = Dotenv.configure()
+                .directory(projectRoot)
+                .load();
+
+        // Registrar cada variable como una propiedad del sistema
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
+}
--- usuario-service/src/main/java/org/unicauca/usuarioservice/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/org/unicauca/usuarioservice/UsuarioServiceApplication.java
@@ -1,7 +1,10 @@
 package org.unicauca.usuarioservice;
 
+import aj.org.objectweb.asm.commons.InstructionAdapter;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.unicauca.usuarioservice.tools.envConfig;
 
 @SpringBootApplication
 public class UsuarioServiceApplication {
